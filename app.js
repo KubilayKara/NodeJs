00aee5df5fc7629d661ec5ad369a4201eebb26ca
semameral <semameral@gmail.com>
2022-07-21 11:54:32 +0100

    events

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-function sayHello(){
-    global.console.log("hello world 3");
-}
+// function sayHello(){
+//     global.console.log("hello world 3");
+// }
 
-sayHello();
+// sayHello();
 
-console.log(module);
+// console.log(module);
 
- const lgr= require("./logger.js");
-lgr("module test");
+//  const lgr= require("./logger.js");
+// lgr("module test");
 
-const pth=require("path");
-console.log(pth);
+// const pth=require("path");
+// console.log(pth);
 
-const fs= require("fs");
-files= fs.readdirSync(".")
+// const fs= require("fs");
+// files= fs.readdirSync(".")
 
-function readdirCallBack(error,file)
-{
-    if(error)
-    console.log(`Error: ${err}`);
-    else 
-    console.log(`success: ${files}`);
-}
+// function readdirCallBack(error,file)
+// {
+//     if(error)
+//     console.log(`Error: ${err}`);
+//     else 
+//     console.log(`success: ${files}`);
+// }
 
-fs.readdir(".",readdirCallBack)
-console.log(files);
+// fs.readdir(".",readdirCallBack)
+// console.log(files);
+
+const EventEmitter=require("events");
+const emitter= new EventEmitter();
+emitter.on("LogGeldi",(args)=>{
+    console.log(`Vallaha da geldi. name:${args.evenName} id:${args.eventId}`);
+});
+
+emitter.emit("LogGeldi",{evenName:"balam", eventId:"1"});
