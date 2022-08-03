4fbba7528ecd93d1cdffd596d8aadf8bcf7f0370
semameral <kara.kubilay@gmail.com>
2022-08-04 00:23:51 +0100

    REST api -> express install

--- webServer.js
+++ webServer.js
@@ -1,13 +1,21 @@
-const http= require("http");
-const server =http.createServer((req,resp)=>{
+const http = require("http");
+const server = http.createServer((req, resp) => {
     console.log("request recieved");
     console.log(req);
+    if (req.url === '/') {
+        resp.write("hello world");
+        resp.end();
+    } else if (req.url==="api/courses"){
+        resp.write("hello api");
+        resp.end();
+    }
+
 
     resp.write("hello");
     resp.end();
 });
 
-server.on("connection", (soket)=>{
+server.on("connection", (soket) => {
     console.log(`connection ${soket}`);
 });
 
