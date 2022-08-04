cf7dc2eb1c76a5a98c6df14e0927c66e1127a48f
semameral <kara.kubilay@gmail.com>
2022-08-04 16:20:56 +0100

    query parameters

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require ('express');
 const app= express();
 require("dotenv").config();
 
-const courses=[{ name: "Web", id: "1001" }, { name: "Rest", id: "1002" }, { name: "Angular", id: "1003" }, { name: "Micro Services", id: "1004" }];
+const courses=[{ name: "Web", id: 1001 }, { name: "Rest", id: 1002 }, { name: "Angular", id: 1003 }, 
+{ name: "Micro Services", id: 1004 }];
 
 //https://expressjs.com/en/4x/api.html#app.get.method
 app.get('/',(req, resp)=>{
@@ -14,20 +15,26 @@ app.get('/api/courses',(req, resp)=>{
     resp.send(courses);
    });
 
-   app.get('/api/courses/:id',(req, resp)=>{
+app.get('/api/courses/:id/:name', (req, resp) => {
     console.log(`id:${req.params.id}`);
-    const res= courses.filter((c)=> {return c.id===req.params.id;});
-    console.log(`result:${res.toString()}`);
-    resp.send(res[0]);
+    console.log(`name:${req.params.name}`);
+    console.log(req.query);
+    // const res= courses.filter((c)=> {return c.id===req.params.id;});
+    // console.log(`result:${res.toString()}`);
+    // resp.send(res[0]);
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    console.log(` course: ${course.name}`);
+    resp.send(course);
    });
 
 
    console.log ("env:");
    console.log(process.env.KUB);
-const port = process.env.PORT || 3000
-app.listen(port, ()=>{
-    console.log (`listening port ${port}`);
-})
+   const port = process.env.PORT || 3000
+   app.listen(port, ()=>{
+       console.log(`listening port ${port}`);
+    })
 // app.post();
 // app.put();
 // app.delete();
