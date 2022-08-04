6e2073b3a7e124a8d9c0f4243e242b29d81b03ae
semameral <kara.kubilay@gmail.com>
2022-08-04 22:23:14 +0100

    REST: validation with Joi

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 //https://www.npmjs.com/package/express
-const { response } = require('express');
-const express = require ('express');
-const app= express();
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+
 app.use(express.json());
 require("dotenv").config();
 
-const courses=[{ name: "Web", id: 1001 }, { name: "Rest", id: 1002 }, { name: "Angular", id: 1003 }, 
+const courses = [{ name: "Web", id: 1001 }, { name: "Rest", id: 1002 }, { name: "Angular", id: 1003 },
 { name: "Micro Services", id: 1004 }];
 
 //https://expressjs.com/en/4x/api.html#app.get.method
-app.get('/',(req, resp)=>{
- resp.send("Hello World From Express")
+app.get('/', (req, resp) => {
+    resp.send("Hello World From Express")
 });
 
-app.get('/api/courses',(req, resp)=>{
+app.get('/api/courses', (req, resp) => {
     resp.send(courses);
-   });  
+});
 
-app.put('/api/courses',(req, resp)=>{
+app.put('/api/courses', (req, resp) => {
     resp.send(courses);
-   });  
-app.post('/api/courses', (req, resp) => { 
-    // console.log("post course body")
-    // console.log(req.body);
-    const course={
-        id: courses.length+1,
-        name: req.body.name
-    };
-    courses.push(course);
-    resp.send(course);
-   });
+});
+app.post('/api/courses', (req, resp) => {
+    // if(!req.body.name|| req.body.name.length<3)
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const result = schema.validate(req.body);
+    console.log(result);
+
+    if (result.error)
+        resp.status(400).send(result.error.details[0].message);
+    else {
+        const course = {
+            id: courses.length + 1,
+            name: req.body.name
+        };
+        courses.push(course);
+        resp.send(course);
+    }
+});
 
 app.get('/api/course/:id', (req, resp) => {
     console.log(`id:${req.params.id}`);
@@ -40,21 +50,21 @@ app.get('/api/course/:id', (req, resp) => {
     // resp.send(res[0]);
 
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course)
+    if (!course)
         resp.status(404).send("Böyle bir kurs yok gülüm");
-    else{
+    else {
         resp.send(course);
         console.log(` course: ${course.name}`);
     }
-   });
+});
 
 
-   console.log ("env:");
-   console.log(process.env.KUB);
-   const port = process.env.PORT || 3000
-   app.listen(port, ()=>{
-       console.log(`listening port ${port}`);
-    })
+console.log("env:");
+console.log(process.env.KUB);
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`listening port ${port}`);
+})
 // app.post();
 // app.put();
 // app.delete();
