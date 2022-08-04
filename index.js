1c750e8fe9d728408658bf4c81744aee36971f80
semameral <kara.kubilay@gmail.com>
2022-08-04 15:59:49 +0100

    REST- url parameters

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const express = require ('express');
 const app= express();
 require("dotenv").config();
 
+const courses=[{ name: "Web", id: "1001" }, { name: "Rest", id: "1002" }, { name: "Angular", id: "1003" }, { name: "Micro Services", id: "1004" }];
+
 //https://expressjs.com/en/4x/api.html#app.get.method
 app.get('/',(req, resp)=>{
  resp.send("Hello World From Express")
 });
 
 app.get('/api/courses',(req, resp)=>{
-    resp.send([{ name: "Web", id: "1001" }, { name: "Rest", id: "1002" }, { name: "Angular", id: "1003" }, { name: "Micro Services", id: "1004" }]);
+    resp.send(courses);
+   });
+
+   app.get('/api/courses/:id',(req, resp)=>{
+    console.log(`id:${req.params.id}`);
+    const res= courses.filter((c)=> {return c.id===req.params.id;});
+    console.log(`result:${res.toString()}`);
+    resp.send(res[0]);
    });
 
 
