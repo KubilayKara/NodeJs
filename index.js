e19390299c7cce389fbe6a370950b1aa7fd666b4
semameral <kara.kubilay@gmail.com>
2022-08-04 22:51:38 +0100

    REST- Put and Delete

--- index.js
+++ index.js
@@ -18,16 +18,39 @@ app.get('/api/courses', (req, resp) => {
     resp.send(courses);
 });
 
-app.put('/api/courses', (req, resp) => {
-    resp.send(courses);
+app.put('/api/courses/:id', (req, resp) => {
+    //look up the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        resp.status(404).send("Böyle bir kurs yok gülüm");
+        return;
+    }
+
+    //Validate
+
+    // const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body);
+    if (error) {
+        resp.status(400).send(error.details[0].message);
+        return;
+    }
+
+    //Update
+    course.name = req.body.name;
+    resp.send(course);
+
 });
-app.post('/api/courses', (req, resp) => {
-    // if(!req.body.name|| req.body.name.length<3)
+
+function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
-    const result = schema.validate(req.body);
-    console.log(result);
+    const res = schema.validate(course);
+    console.log(res);
+    return res;
+}
+app.post('/api/courses', (req, resp) => {
+    const result = validateCourse(req.body);
 
     if (result.error)
         resp.status(400).send(result.error.details[0].message);
@@ -41,6 +64,17 @@ app.post('/api/courses', (req, resp) => {
     }
 });
 
+app.delete("/api/courses/:id", (req, resp) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return resp.status(404).send("Böyle bir kurs yok gülüm");
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    resp.send(course);
+});
+
 app.get('/api/course/:id', (req, resp) => {
     console.log(`id:${req.params.id}`);
     // console.log(`name:${req.params.name}`);
@@ -51,11 +85,12 @@ app.get('/api/course/:id', (req, resp) => {
 
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course)
-        resp.status(404).send("Böyle bir kurs yok gülüm");
-    else {
-        resp.send(course);
-        console.log(` course: ${course.name}`);
-    }
+        return resp.status(404).send("Böyle bir kurs yok gülüm");
+
+
+    resp.send(course);
+    console.log(` course: ${course.name}`);
+
 });
 
 
